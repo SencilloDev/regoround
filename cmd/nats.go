fe36f8ee25ac2c123bb81ddc3cc2fc71a79f6cf3
John Hooks <hooksie11@gmail.com>
2025-05-14 17:10:41 -0400

    add license

--- cmd/nats.go
+++ cmd/nats.go
@@ -1,30 +1,44 @@
-package cmd 
+// Copyright 2025 Sencillo
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package cmd
 
 import (
 	"log/slog"
 	"os"
 
-        "github.com/nats-io/jsm.go/natscontext"
-        "github.com/nats-io/nats.go"
-        "github.com/spf13/viper"
+	"github.com/nats-io/jsm.go/natscontext"
+	"github.com/nats-io/nats.go"
+	"github.com/spf13/viper"
 )
 
 func newNatsConnection(name string) (*nats.Conn, error) {
-        opts := []nats.Option{nats.Name(name)}
+	opts := []nats.Option{nats.Name(name)}
 
-        _, ok := os.LookupEnv("USER")
+	_, ok := os.LookupEnv("USER")
 
-        if viper.GetString("credentials_file") == "" && viper.GetString("nats_jwt") == "" && ok {
-                slog.Debug("using NATS context")
-                return natscontext.Connect("", opts...)
-        }
+	if viper.GetString("credentials_file") == "" && viper.GetString("nats_jwt") == "" && ok {
+		slog.Debug("using NATS context")
+		return natscontext.Connect("", opts...)
+	}
 
-        if viper.GetString("nats_jwt") != "" {
-                opts = append(opts, nats.UserJWTAndSeed(viper.GetString("nats_jwt"), viper.GetString("nats_seed")))
-        }
-        if viper.GetString("credentials_file") != "" {
-                opts = append(opts, nats.UserCredentials(viper.GetString("credentials_file")))
-        }
+	if viper.GetString("nats_jwt") != "" {
+		opts = append(opts, nats.UserJWTAndSeed(viper.GetString("nats_jwt"), viper.GetString("nats_seed")))
+	}
+	if viper.GetString("credentials_file") != "" {
+		opts = append(opts, nats.UserCredentials(viper.GetString("credentials_file")))
+	}
 
-        return nats.Connect(viper.GetString("nats_urls"), opts...)
+	return nats.Connect(viper.GetString("nats_urls"), opts...)
 }
